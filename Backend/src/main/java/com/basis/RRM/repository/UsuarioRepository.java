0b127d4f0e9e9d473ae3caef9e3dde2a6a37c156
Théo Gallo <gallotheo@icloud.com>
2021-09-21 16:07:24 -0300

    configuração de email e filtragem de usuario

--- Backend/src/main/java/com/basis/RRM/repository/UsuarioRepository.java
+++ Backend/src/main/java/com/basis/RRM/repository/UsuarioRepository.java
@@ -2,12 +2,13 @@ package com.basis.RRM.repository;
 
 import com.basis.RRM.dominio.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>, JpaSpecificationExecutor<Usuario> {
 
     List<Usuario> findByStatusTrue();
     boolean existsByCpf(String cpf);
--- Backend/src/main/java/com/basis/RRM/web/rest/UsuarioResource.java
+++ Backend/src/main/java/com/basis/RRM/web/rest/UsuarioResource.java
@@ -4,6 +4,7 @@ package com.basis.RRM.web.rest;
 import com.basis.RRM.service.UsuarioService;
 import com.basis.RRM.service.dto.UsuarioDTO;
 import com.basis.RRM.service.dto.UsuarioListagemDTO;
+import com.basis.RRM.service.filter.UsuarioFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,5 +44,11 @@ public class UsuarioResource {
         usuarioService.inativar(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @GetMapping("filtro")
+    public ResponseEntity<List<UsuarioListagemDTO>> exibirUsuariosFiltrado(@RequestBody UsuarioFilter usuario){
+        return ResponseEntity.ok(usuarioService.mostrarTodosUsuariosFiltrado(usuario));
+    }
+
 }
 
