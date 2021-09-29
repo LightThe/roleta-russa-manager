ed3250f299243e4fa4e883673d045ecdef85449c
Théo Gallo <gallotheo@icloud.com>
2021-09-29 14:34:16 -0300

    Rotas de usuario listagem

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,13 +13,15 @@ import { PageNotificationModule, BreadcrumbModule, MenuModule, ErrorStackModule 
 import { ErrorModule, SecurityModule, VersionTagModule } from '@nuvem/angular-base';
 import { DiarioErrosComponent } from './components/diario-erros/diario-erros.component';
 import { BlockUIModule } from 'ng-block-ui';
+import { UsuarioReadComponent } from './components/usuario-read/usuario-read.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         AppTopbarComponent,
         AppFooterComponent,
-        DiarioErrosComponent
+        DiarioErrosComponent,
+        UsuarioReadComponent,
     ],
     imports: [
         BlockUIModule.forRoot({
